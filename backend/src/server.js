eafc011253a5c1b928322d78af464095521dc833
khanhhoang209 <lehoangkhanh209@gmail.com>
2025-09-26 23:39:13 +0700

    feat: init commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express, { request, response } from "express";
+import tasksRouters from "./routes/tasksRouters.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/tasks", tasksRouters);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
